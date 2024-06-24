8a298ea7d6934774b8dc808e76c2e6c21163599b
Travis <unkletas@gmail.com>
2024-06-24 08:48:11 -0500

    added col and row classes to Tailwinds safelist

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [
+    "col-span-1",
+    "col-span-2",
+    "col-span-3",
+    "col-span-4",
+    "col-span-5",
+    "col-span-6",
+    "col-span-7",
+    "col-span-8",
+    "row-span-1",
+    "row-span-2",
+    "row-span-3",
+    "row-span-4",
+    "row-span-5",
+    "row-span-6",
+    "row-span-7",
+    "row-span-8",
+  ],
   theme: {
     extend: {
       colors: {
