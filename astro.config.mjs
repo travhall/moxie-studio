5c100bee71009d39f01900826808010c23355229
Travis <unkletas@gmail.com>
2024-09-13 22:51:24 -0500

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
