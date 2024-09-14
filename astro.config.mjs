55a7810961a02a6be1e619c4e9a4b9dcb65bd04d
Travis <unkletas@gmail.com>
2024-09-13 22:43:07 -0500

    manual rebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import devtoolbarTailwind from "astro-devtoolbar-tailwind";
 import icon from "astro-icon";
-import lighthouse from "astro-lighthouse";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [devtoolbarTailwind(), icon(), tailwind(), lighthouse()],
+  integrations: [tailwind(), icon()],
   output: "server",
   adapter: netlify(),
 });
